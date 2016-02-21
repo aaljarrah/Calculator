1d2c8289d8ee1d10f7fce267f2966c22f5a52912
Abdullah Aljarrah <marteno_95@hotmail.com>
2016-02-21 16:26:18 -0700

    Initial Version.

---
+++ cse360assign3/src/cse360assign3/Calculator.java
@@ -0,0 +1,78 @@
+/** Description of Calculator
+ *  This class has as names is a calculator than can
+ *  sum, add, subtract, multiply and divide. It also
+ *  can return the history of your calculations.
+*
+* @author Abdullah Aljarrah
+* @pin 105
+* @version Feb 21, 2016.
+*/
+
+package cse360assign3;
+
+public class Calculator {
+	
+	private int total;
+	
+	/** Description of Calculator()
+	 *  This is the constructor of class Calculator where it
+	 *  initializes value total to 0.
+	 *  */
+	
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/** Description of getTotal()
+	 *  */
+	
+	public int getTotal () {
+		return 0;
+	}
+	
+	/** Description of add(int value)
+	 * 
+	 * @param value
+	 *  */
+	
+	public void add (int value) {
+		
+	}
+	
+	/** Description of subtract(int value)
+	 * 
+	 * @param value
+	 *  */
+	
+	public void subtract (int value) {
+		
+	}
+	
+	/** Description of multiply(int value)
+	 * 
+	 * @param value
+	 *  */
+	
+	public void multiply (int value) {
+		
+	}
+	
+	/** Description of divide(int value) 
+	 * 
+	 * @param value
+	 *  */
+	
+	public void divide (int value) {
+		
+	}
+	
+	/** Description of getHistory()
+	 * 
+	 * @return
+	 *  */
+	
+	public String getHistory () {
+		return "";
+	}
+
+}
